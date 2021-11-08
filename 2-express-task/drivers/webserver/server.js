c90eedcb0a5058c0986a45ba06e9acc5dbdd42f7
Romi <romi@qlue.id>
2021-11-08 15:17:26 +0700

    [Express Task] - Change env port express name

--- 2-express-task/drivers/webserver/server.js
+++ 2-express-task/drivers/webserver/server.js
@@ -21,7 +21,7 @@ app.use((err, req, res, next) => {
   }
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT_EXPRESS || 3000
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT: ${PORT}`);
